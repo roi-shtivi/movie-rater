354ccd7b84cfbce91ca529e37b4f572a9e483574
roi1255 <roi.shtivi@mail.huji.ac.il>
2018-11-10 10:37:58 +0200

    init

---
+++ movie.py
@@ -0,0 +1,25 @@
+class Movie:
+    def __init__(self, code, name, rate, votes):
+        self.code = code
+        self.name = name
+        self.rate = rate
+        self.votes = votes
+        self.dates = []
+
+    def add_date(self, date):
+        self.dates.append(date)
+
+    def set_rate(self, rate):
+        self.rate = rate
+
+    def set_voters(self, votes):
+        self.votes = votes
+
+    def __repr__(self):
+        return "{}: {}/10 ({})".format(self.name, self.rate, self.votes)
+
+    def __eq__(self, other):
+        return self.rate == other.rank and self.votes == other.votes
+
+    def __lt__(self, other):
+        return self.rate < other.rate
